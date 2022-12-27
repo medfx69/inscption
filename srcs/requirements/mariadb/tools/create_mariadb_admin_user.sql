edf40a88288eba8b96360f3f4f2b614817f22790
mohamed <mohamedox9080@gmail.com>
2022-12-27 17:52:25 +0100

    updates

---
+++ srcs/requirements/mariadb/tools/create_mariadb_admin_user.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER IF NOT EXISTS 'mait-aad'@'%' IDENTIFIED BY 'Mmedddaaa';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'mait-aad'@'%';
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';
