261925bd52e6aef0dd6da4af7a0859bf6939f02e
mohamed <mohamedox9080@gmail.com>
2022-12-28 01:10:06 +0100

    updates

--- srcs/requirements/mariadb/tools/create_mariadb_admin_user.sql
+++ srcs/requirements/mariadb/tools/create_mariadb_admin_user.sql
@@ -2,4 +2,4 @@ CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER IF NOT EXISTS 'mait-aad'@'%' IDENTIFIED BY 'Mmedddaaa';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'mait-aad'@'%';
 FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'Mmedddaaa';
--- srcs/requirements/wordpress/tools/wp-config.php
+++ srcs/requirements/wordpress/tools/wp-config.php
@@ -1,13 +1,54 @@
+
 <?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv("MYSQL_DB_NAME"));
-/** MySQL database username */
-define( 'DB_USER', getenv("MYSQL_ADMIN_USER"));
-/** MySQL database password */
-define( 'DB_PASSWORD', getenv("MYSQL_ADMIN_PASS"));
+define( 'DB_NAME', 'wordpress' );
+
+/** Database username */
+define( 'DB_USER', 'mait-aad' );
+
+/** Database password */
+define( 'DB_PASSWORD', 'Mmedddaaa' );
+
+/** Database hostname */
+define( 'DB_HOST', '0.0.0.0' );
+
+/** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
+
+/** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
-$table_prefix = 'wp_';
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
 define('AUTH_KEY',         'ip1;QY{-.NZ/+%UaeD<U#:8u+Di,t8Jua`-+oJ{[P_[#vgjv:Nf1G5R#(tlz$dT;');
 define('SECURE_AUTH_KEY',  '!}dB/wT0GKbWzy+(2N>]x(mnJm~d9=AeAqr{ ]Ifz,$:+h?tD.VA0IhoumWhywo9');
 define('LOGGED_IN_KEY',    '0I_I>{ MKQecw).6a&F |+h76H9u9r(7(E{I< |+Oo]e|OP-W ^NHQMb5DZ+sGco');
@@ -16,11 +57,40 @@ define('AUTH_SALT',        '64dZ{.11D.#4P2jZHEV=OK#^||9&i2%ig3~/+e_e2S7Y@|K%H=UK
 define('SECURE_AUTH_SALT', 'lQj_!?vZObJ9ID#Qtr}@%wD0F.&@`}(Z015i$^]HP@/f>3Fv8&94t6%rP~&,/>- ');
 define('LOGGED_IN_SALT',   ' C|||sJvGF?++&2STZEgLv;~3m1}D]%!S-r7jX2|{JBb`>-BeO&v0iQxD-]fz?=>');
 define('NONCE_SALT',       '1aLQu+=01n$Sr2(s8M+)pYe1,aFeQMLo3k%aOHBw>efr8Is5$8R;x[zZH~!ZWAFw');
+/**#@-*/
 
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+        define( 'ABSPATH', __DIR__ . '/' );
 }
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
-?>
